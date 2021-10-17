d0a07044f66b93a952cf232dff5bb28dad84c6d0
Mauro Guillermo Torres Velasquez <maurotv50@gmail.com>
2021-10-17 04:45:10 -0300

    Agregado de cpp game

---
+++ game.cpp
@@ -0,0 +1,41 @@
+
+
+class Game
+{
+private:
+
+public:
+	Game();
+	~Game();
+
+	void init();
+	void update();
+	void draw();
+	void close();
+};
+
+Game::Game()
+{
+
+}
+Game::~Game()
+{
+
+}
+
+void Game::init()
+{
+
+}
+void Game::update()
+{
+
+}
+void Game::draw()
+{
+
+}
+void Game::close()
+{
+
+}
